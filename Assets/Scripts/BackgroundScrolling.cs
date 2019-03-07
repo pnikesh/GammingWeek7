76c970314c0aa8071a1c793bdf1d6d06552cc12a
Nik <nikeshpatel356@gmail.com>
2019-03-06 21:53:21 -0500

    EnWeek8-Added MainMenu and floating backgrouind

---
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScrolling : MonoBehaviour
+{
+    public float scrollSpeed = 0.1f;
+
+    /*void Start(){
+        InvokeRepeating("MoveBG", 0.5f, 0.5f);
+    }
+
+    void MoveBG(){
+         GetComponent<Renderer>().material.mainTextureOffset = 
+            new Vector2(GetComponent<Renderer>.material.mainTextureOffset.x + scrollSpeed, 0);
+    }*/
+
+    // Update is called once per frame
+     void Update()
+    {
+        
+        /*GetComponent<Renderer>().material.mainTextureOffset = 
+            new Vector2(GetComponent<Renderer>.material.mailTextureOffset.x + scrollSpeed, 0);*/
+
+         GetComponent<Renderer>().material.mainTextureOffset = 
+            new Vector2(Time.time * scrollSpeed, 0);
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    //what happens when i click on START button
+    public void StartGame(){
+        SceneManager.LoadScene("Week6");
+    }
+
+    //what happens when ii click QUIT button 
+    public void QuitGame(){
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+        
+    }
+}
